68b341c6bebd65991c3653f52fe02c8445e54809
Yehoshua Oliveira <main@ioxua.com>
2024-02-08 21:35:54 -0300

    feat: changes build

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,8 +1,9 @@
 import pug from 'pug'
 import fs from 'fs/promises'
+import { dirname } from 'path'
 import { glob } from 'glob'
 
-const pugFiles = await glob('src/*.pug', {
+const pugFiles = await glob('src/**/*.pug', {
   ignore: 'src/_*.pug'
 })
 
@@ -10,13 +11,22 @@ console.log('Pug files:', pugFiles)
 
 for (const file of pugFiles) {
   const compiled = pug.compileFile(file)
+
+  // Convert filename (src/index.pug) to dist (dist/index.html)
   const dist = file
     .replace('src', 'dist')
     .replace('pug', 'html')
 
   const html = compiled()
+
+  // Get dirname (dist) from filename (dist/index.html)
+  const dir = dirname(dist)
+
+  // Create dist dir if it doesn't exist
+  await fs.mkdir(dir, { recursive: true })
   
-  fs.writeFile(dist, html, { 
+  // Write html file
+  await fs.writeFile(dist, html, { 
     encoding: 'utf-8',
   })
 }
